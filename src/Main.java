4f31ac254d6708a8fc517a01452f22cfa3183ee4
Bektur <Bekturhanazarov98@gmail.com>
2023-10-10 15:32:14 +0600

    asdasdsa

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Daniel");
+        System.out.println("Hello Beka");
     }
 }
+// git init
+// git add .
+// git commit -m "My commit"
+// git remote add origin "https://github.com/beka3301/daniel12142007.git"
+// git push -u origin master
+
+// git add .
+// git commit -m "My commit"
+// git push -u origin master
